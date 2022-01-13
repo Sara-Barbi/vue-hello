b963ced730b8df32c8376c5152d94a038f269475
Sara-Barbi <barbisara02@gmail.com>
2022-01-13 15:42:34 +0100

    saluto

---
+++ main.js
@@ -0,0 +1,9 @@
+const salutoUtente = new Vue (
+    {
+       el : "#root",
+       data : {
+           saluto:"ciao utente,questo messaggio è stato stampato grazie a Vue"
+          
+       }
+    }
+);
