9f6283ed56761fd4b5566b7b0a4092a5c6b6913d
Sara-Barbi <barbisara02@gmail.com>
2022-01-13 16:15:31 +0100

    end

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const salutoUtente = new Vue (
     {
        el : "#root",
        data : {
-           saluto:"ciao utente,questo messaggio è stato stampato grazie a Vue"
-          
+           saluto:"ciao utente,questo messaggio è stato stampato grazie a Vue",
+
+           immagine:"https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg",
        }
     }
 );
